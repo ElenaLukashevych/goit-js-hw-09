03d513fabc271c82cdfbb88363f722c8a9977d96
ElenaLukashevych <elukashevych@gmail.com>
2022-03-10 16:03:24 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+
+  timerId = setInterval(() => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+      startBtn.setAttribute("disabled", true);
+};
+
+
+function onStopClick() {
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+};
+
+
+
+
+
 
